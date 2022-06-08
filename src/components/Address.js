b6f4a140a805510da473ff7be137fcbd09876653
chanakya213 <chanakya213@.com>
2022-06-08 21:19:20 +0530

    ok

--- src/components/Address.js
+++ src/components/Address.js
@@ -5,7 +5,7 @@ const Address = () => {
     <>
       <div className="details d-flex flex-wrap container-fluid mt-3 w-100 justify-content-between pb-3">
         <div className="mainContainer">
-          <div className="d-flex flex-nowrap justify-content-between mt-3">
+          <div className="d-flex flex-nowrap justify-content-between mt-3 px-2">
             <h3 className="text-dark">Contact Details</h3>
             <h3>Edit</h3>
           </div>
@@ -18,7 +18,7 @@ const Address = () => {
             </div>
             <div className="d-flex smallBoxes h-20 bg-light p-3">Phone</div>
           </div>
-          <div className="d-flex w-100 h-20 justify-content-between mt-3">
+          <div className="d-flex w-100 h-20 justify-content-between mt-3 ">
             <div className="d-flex smallBoxes h-20 bg-light p-3">Male</div>
             <div className="d-flex smallBoxes h-20 bg-light p-3">
               Date Of Birth :
@@ -29,7 +29,7 @@ const Address = () => {
           </div>
         </div>
         <div className="mainContainer">
-          <div className="d-flex flex-nowrap justify-content-between mt-3">
+          <div className="d-flex flex-nowrap justify-content-between mt-3 px-2">
             <h3 className="text-dark">Address Details</h3>
             <h3>Edit</h3>
           </div>
